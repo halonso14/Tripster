c750b837685996989da94566f0cc6582c394b8e8
chachooon <chachooon@gmail.com>
2017-12-10 15:21:47 +0900

    TripsterProject 폴더삭제

--- TripsterProject/src/main/java/com/Tripster/controller/test.java
+++
@@ -1,5 +0,0 @@
-package com.Tripster.controller;
-
-public class test {
-
-}
--- TripsterProject/src/main/java/com/Tripster/service/ScrapService.java
+++
@@ -1,9 +0,0 @@
-package com.Tripster.service;
-
-import com.Tripster.domain.ScrapVO;
-
-public interface ScrapService {
-	
-	public void regist(ScrapVO board)throws Exception;
-		
-}
