4261ad10aa9a5530f695365090efd7a856765a3c
chachooon <chachooon@gmail.com>
2018-01-02 09:03:30 +0900

    .

--- src/main/java/com/tripster/domain/EsSearchResult.java
+++ src/main/java/com/tripster/domain/EsSearchResult.java
@@ -2,13 +2,13 @@ package com.tripster.domain;
 
 import java.util.*;
 
+// 통합 검색결과 값 한번에 담기위한 객체 (contentsVO + planVO + memberVO)
 public class EsSearchResult {
-
 	
 	public List<EsContentsVO> contentsList;
 
 	public List<EsPlanVO> planList;
 
 	public List<EsMemberVO> memberList;
-	
+
 }
