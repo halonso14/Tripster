2beba0f40382274d895e86fb63451f568a98a22e
chachooon <chachooon@gmail.com>
2018-01-03 12:31:09 +0900

    .

--- src/main/java/com/tripster/elasticsearch/EsConfig.java
+++ src/main/java/com/tripster/elasticsearch/EsConfig.java
@@ -3,7 +3,7 @@ package com.tripster.elasticsearch;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
 
-import java.net.InetAddress;
+import java.net.InetSocketAddress;
 
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.settings.Settings;
@@ -17,10 +17,7 @@ public class EsConfig {
     public Client client() throws Exception {
         Settings settings = Settings.builder()
                 .put("cluster.name",  "my-application").build();
-        
-       
-        return new PreBuiltTransportClient(settings)
-                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getLocalHost(), 9300));
 
+        return new PreBuiltTransportClient(settings).addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress("192.168.0.3", 9300)));
     }
 }
