6e972d2d27f9a62d24fcac86ab3bab6c4846f0ea
chachooon <chachooon@gmail.com>
2018-01-05 10:48:16 +0900

    다합쳤다

--- src/main/java/com/tripster/interceptorSample/SampleInterceptor.java
+++
@@ -1,9 +0,0 @@
-package com.tripster.interceptorSample;
-
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
-public class SampleInterceptor extends HandlerInterceptorAdapter{
-	
-	
-
-}
