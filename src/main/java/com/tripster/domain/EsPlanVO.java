668f3e658c5495edcc57e1a2ef8faa6952508c08
kevin <kevin910310@gmail.com>
2018-01-08 17:37:40 +0900

    0108 합치고 에러수정

--- src/main/java/com/tripster/domain/EsPlanVO.java
+++ src/main/java/com/tripster/domain/EsPlanVO.java
@@ -1,13 +1,11 @@
-//package com.tripster.domain;
-//
-//import java.util.List;
-//
-//import com.fasterxml.jackson.annotation.JsonFilter;
-//
+package com.tripster.domain;
+
+import java.util.List;
+
 //@JsonFilter("EsPlanFilter")
-//public class EsPlanVO extends PlanVO{
-//	
-//	private PlanDetailVO planDetail;
-//	
-//	private List<PlanReplyVO> replies;
-//}
+public class EsPlanVO extends PlanVO{
+	
+	private PlanDetailVO planDetail;
+	
+	private List<PlanReplyVO> replies;
+}
