5456c53dd57e21e94cb2a4c277161b3d39eddcb9
halonso14 <hdsbsb@naver.com>
2018-01-31 14:45:23 +0900

    컨텐츠 디테일 페이지 수정 스크랩 버튼 구글맵

---
+++ src/main/webapp/resources/assets/js/initialize-google-mapHD.js
@@ -0,0 +1,12 @@
+	function loadScript() {
+	    setTimeout(function (){
+		  $('#map-canvas').css({'display':'block'});
+		  var script = document.createElement('script');
+		  script.type = 'text/javascript';
+		  script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCb1-Itd2i_3RlBDhbMBYhb9uL6V7HdX6g&callback=initialize';
+		  document.body.appendChild(script);
+		  
+//		  google.maps.event.trigger(map, 'resize');
+	    }, 500);	
+	}
+	
--- src/main/webapp/resources/js/scrapHD.js
+++ src/main/webapp/resources/js/scrapHD.js
@@ -2,16 +2,19 @@ function mouseover(memberID, $button){
 	var contentsID = $button.val();
 	//로그인을 하지 않은 경우
 	if(memberID == ""){
-		$button.on('click', function() {
+		$button.unbind('click').bind('click', function() {
 			alert("로그인이 필요한 기능입니다.");
 			return;
 		});
 	} else {
-		if ($button.hasClass('unscrap')) {
+		//초록색 버튼
+		if ($button.hasClass('scraped')) {
+			//초록색 -> 빨간색
+			$button.addClass('unscrap');
+			$button.text('스크랩 취소');
 			$button.unbind('mouseleave').bind('mouseleave', function() {
 				$button.removeClass('unscrap');
 				$button.text('스크랩');
-				return false;
 			});
 			$button.unbind('click').bind('click', function() {
 				//빨간색 -> 흰색
@@ -19,36 +22,15 @@ function mouseover(memberID, $button){
 				$button.removeClass('scraped');
 				$button.text('스크랩');
 				$.post("/scrapDelete/"+contentsID);
-				return false;
 			});
-		} else {
-			if ($button.hasClass('scraped')) {
-				$button.addClass('unscrap');
-				$button.text('스크랩 취소');
-				$button.unbind('mouseleave').bind('mouseleave', function() {
-					$button.removeClass('unscrap');
-					$button.text('스크랩');
-					return false;
-				});
-				$button.unbind('click').bind('click', function() {
-					//빨간색 -> 흰색
-					$button.removeClass('unscrap');
-					$button.removeClass('scraped');
-					$button.text('스크랩');
-					$.post("/scrapDelete/"+contentsID);
-					return false;
-				});
-			} else{
-				$button.unbind('click').bind('mouseleave', function() {
-					return false;
-				});
-				$button.unbind('click').bind('click', function() {
-					$.post("/scrapDelete/"+contentsID);
-					$button.addClass('scraped');
-					$button.text('스크랩');
-					return false;
-				});
-			}
+		} else{
+			$button.unbind('click').bind('mouseleave', function() {
+			});
+			$button.unbind('click').bind('click', function() {
+				$.post("/scrapDelete/"+contentsID);
+				$button.addClass('scraped');
+				$button.text('스크랩');
+			});
 		}
 	}
 }
